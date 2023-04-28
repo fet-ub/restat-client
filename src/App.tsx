a65255b8a26ac6b2fb191e3cce390afe6ee19148
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-04-28 19:29:00 +0100

    add packages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-400">
+      <h1 className="text-3xl font-bold text-primary">
         Hello world!
       </h1>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screen: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        primary: "#3B82F6",
+        bluishColor: "#CBD5E1",
+        darkBlue: "#0F172A",
+        brownColor: "#CBD5E1",
+      },
+    },
   },
   plugins: [],
 };
