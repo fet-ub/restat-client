f35ef58346cf296e5d3621dd92d899aa39a5d42f
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-05-01 16:37:33 +0100

    add login logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,41 @@ import React from "react";
 import "./App.css";
 import InputField from "./components/login/InputField";
 import CustomButton from "./components/common/CustomButton";
+import {useForm} from "react-hook-form"
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
 
 
 function App() {
+
+  const schema = yup.object().shape({
+    username:yup.string().required("Username is required"),
+    password: yup.string().required("Password is required")
+  })
+   type FormData = yup.InferType<typeof schema>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver:yupResolver(schema)
+  });
+
+ 
+console.log(errors)
+
+  const loginUser=(data:FormData)=>{
+    console.log(data)
+  }
   return (
     <div className="grid grid-cols-12 h-screen ">
       <div className="col-span-6 flex flex-col justify-center items-center bg-bluishColor">
         <div className="mt-8">
           <img
             src="./female.png"
-            className="w-[450px] h-auto"
+            className="w-[380px] h-auto"
             alt="illustration"
           />
         </div>
@@ -35,28 +60,33 @@ function App() {
           <h1 className="text-4xl font-bold">
             Welcome to <span className="text-brandColor">Restat</span>
           </h1>
-          <form className="w-[65%]">
+          <form className="w-[65%]" onSubmit={handleSubmit(loginUser)}>
             <div className="mt-12">
               <InputField
                 labelTitle="Username"
                 placeholderTitle="Enter your username"
                 inputType="text"
+                inputFieldData={register("username")}
               />
             </div>
-            <div className="mt-8">
+            <p className="text-red-500 mt-1">{errors.username?.message}</p>
+            <div className="mt-7">
               <InputField
                 labelTitle="Password"
                 placeholderTitle="Enter your password"
                 inputType="password"
+                inputFieldData={register("password")}
               />
             </div>
+            <p className="text-red-500 mt-1">{errors.password?.message}</p>
             <div className="text-right mt-3 mb-3">
               <p className="font-medium text-brandColor text-[17px]">
                 Forgot Password?
               </p>
             </div>
+
             <div>
-              <CustomButton/>
+              <CustomButton />
             </div>
           </form>
         </div>
--- src/components/login/InputField.tsx
+++ src/components/login/InputField.tsx
@@ -4,9 +4,10 @@ interface InputFieldProps{
     labelTitle: string;
     placeholderTitle: string;
     inputType: string;
+    inputFieldData: any;
 }
 
-const InputField = ({labelTitle,placeholderTitle,inputType}:InputFieldProps) => {
+const InputField = ({labelTitle,placeholderTitle,inputType,inputFieldData}:InputFieldProps) => {
   return (
     <>
       <label className="text-xl font-semibold ">{labelTitle}</label>
@@ -15,6 +16,7 @@ const InputField = ({labelTitle,placeholderTitle,inputType}:InputFieldProps) => 
         type={inputType}
         className="w-full mt-2  pl-5  py-3 font-normal placeholder-brownColor   border rounded-md ring-1 ring-brownColor placeholder:font-normal placeholder:text-[18px] focus:outline-none "
         placeholder={placeholderTitle}
+        {...inputFieldData}
       />
     </>
   );
