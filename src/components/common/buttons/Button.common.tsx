968a10bdb8ba6a445d0f1c692e0d61e5bef7c9b5
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-05-23 20:53:48 +0100

    add button component

---
+++ src/components/common/buttons/Button.common.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ButtonTypes } from '../../../types/common/button.type'
+
+const Button = ({text,buttonType,width,fullWidth,disable,onClick,icon}:ButtonTypes) => {
+  return (
+    <button
+      className={`${
+        buttonType === "PRIMARY"
+          ? "bg-primary text-white"
+          : buttonType === "SECONDARY"
+          ? "bg-transparent border border-primary text-primary"
+          : buttonType === "TERTIARY"
+          ? "bg-transparent border text-secondary border-slate-300"
+          : buttonType === "ERROR"
+          ? "bg-errorColor text-white"
+          : "text-primary"
+      }
+     py-[10px] px-5 border rounded-lg outline-none text-[16px] flex justify-center items-center `}
+      onClick={onClick}
+      disabled={disable}
+      style={{
+        cursor:
+          disable === true  ? "not-allowed" : "pointer",
+        width:
+          fullWidth === true
+            ? "100%"
+            : width !== undefined
+            ? width
+            : "fit-content",
+      }}
+    >
+        {
+            icon?<span className='flex justify-center items-center'>{icon}</span>:''
+        }
+      {text}
+    </button>
+  );
+}
+
+export default Button
