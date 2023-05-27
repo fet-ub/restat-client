665202f7b4403a5392b95050134c62d5b37cc7bc
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-05-27 20:32:42 +0100

    select input

---
+++ src/components/common/inputs/select-input/SelectInput.common.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { SelectInputPropType } from '../../../../types/common/select-input.type';
+import styles from '../input.module.css'
+
+const SelectInput = ({
+  selectOptions,
+  onChange,
+  value,
+  label,
+}: SelectInputPropType) => {
+  return (
+    <div className="mb-4">
+      <label className="block  text-[17px] text-secondary  font-[500] mb-2">
+        {label}
+      </label>
+      <select
+        value={value}
+        className={styles.select__container}
+        onChange={onChange}
+      >
+        {selectOptions?.map((selectOption, index) => {
+          return (
+            <option value={selectOption.value} key={index}>
+              {selectOption.label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectInput
---
+++ src/repository/constants/constants.ts
@@ -0,0 +1,10 @@
+export const LANGUAGE_CONSTANTS = [
+  {
+    label: "English",
+    value: "english",
+  },
+  {
+    label: "French",
+    value: "french",
+  },
+];
