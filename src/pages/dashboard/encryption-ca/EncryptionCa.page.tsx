ecca290da7efeda72324353397bed06fc74651fb
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-06-27 09:43:10 +0100

    add encryption

--- src/pages/dashboard/encryption-ca/EncryptionCa.page.tsx
+++ src/pages/dashboard/encryption-ca/EncryptionCa.page.tsx
@@ -1,33 +1,131 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardHeader from '../../../components/common/dashboard-header/DashboardHeader.common';
 import { IconRepository } from '../../../repository/icons/icon.repository';
 import EncryptCaTable from '../../../components/common/table/encrypt-ca/EncryptCaTable.common';
+import * as xlsx from "xlsx";
 
 const EncryptionCaPage = () => {
   // const [isOpen, setIsOpen] = useState(false);
   const [active, setActive] = useState(0);
 
-    const [selectedFile, setSelectedFile] = useState<File | undefined>(
-      undefined
-    );
+  const [selectedFile, setSelectedFile] = useState<any>([]);
+  const [marksTableData, setMarksTableData] = useState<any>([]);
 
+  const [fileName, setFileName] = useState<any>("");
 
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (e?.target?.files) {
-        //  setSelectedCategoryImage(URL.createObjectURL(e.target.files[0]));
-        setSelectedFile(e.target.files[0]);
-        console.log(e.target.files[0]);
+  const handleFileChange = async (e: any) => {
+    if (e?.target?.files) {
+      //  setSelectedCategoryImage(URL.createObjectURL(e.target.files[0]));
+      // setSelectedFile(e.target.files[0]);
+      setFileName(e.target.files[0]);
+      // console.log(e.target.files[0]);
 
-        console.log(selectedFile?.name);
-      }
-    };
+      // console.log(selectedFile?.name);
+      const file = e.target.files[0];
+      const data = await file.arrayBuffer(file);
+      const excelfile = xlsx.read(data);
+      const excelsheet = excelfile.Sheets[excelfile.SheetNames[0]];
+      const exceljson = xlsx.utils.sheet_to_json(excelsheet);
+      // console.log(exceljson);
+      setSelectedFile(exceljson);
+      // console.log('zxcxzc',selectedFile);
+      
+    }
+  };
 
-    const handleXls = () => {
-      setActive(0);
-    };
-    const handleCSV = () => {
-      setActive(1);
-    };
+  useEffect(() => {
+    for (let i = 0; i < selectedFile.length; i++) {
+      const keys = Object.keys(selectedFile[i]);
+      const regex1 = /matricule/i;
+      const regex2 = /ca/i;
+      const filteredKeys = keys.filter(
+        (key) => regex1.test(key) || regex2.test(key)
+      );
+      filteredKeys.sort((a, b) => {
+        if (regex1.test(a)) {
+          return -1;
+        } else if (regex1.test(b)) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      const result = selectedFile.reduce((acc: any, curr: any) => {
+        const obj: any = {};
+        filteredKeys.forEach((key) => {
+          let newKey = key;
+          if (regex1.test(key)) {
+            newKey = "matricule";
+          } else if (regex2.test(key)) {
+            newKey = "ca";
+          }
+          obj[newKey] = curr[key];
+        });
+        obj.status = "not filled";
+        obj.encrypt = "";
+        acc.push(obj);
+        return acc;
+      }, []);
+
+      setMarksTableData(result);
+    }
+  }, [selectedFile]);
+
+  //  console.log("zxcxzc", selectedFile);
+
+  // interface ObjectType{
+  //   [key: string]: string;
+  // }
+
+  // for (let i:number = 0; i < selectedFile.length; i++) {
+  //   const keys = Object.keys(selectedFile[i] );
+  //   // console.log(keys); 
+  //   const regex1 = /matricule/i;
+  //   const regex2 = /ca/i;
+  //   const filteredKeys = keys.filter(
+  //     (key) => regex1.test(key) || regex2.test(key)
+  //   );
+  //    filteredKeys.sort((a, b) => {
+  //      if (regex1.test(a)) {
+  //        return -1;
+  //      } else if (regex1.test(b)) {
+  //        return 1;
+  //      } else {
+  //        return 0;
+  //      }
+  //    });
+  //   // console.log(filteredKeys);
+  //   // ["name", "age"]
+
+  //   const result = selectedFile.reduce((acc:any, curr:any) => {
+  //     const obj:any = {};
+  //     filteredKeys.forEach((key) => {
+  //      let newKey = key;
+  //      if (regex1.test(key)) {
+  //        newKey = "matricule";
+  //      } else if (regex2.test(key)) {
+  //        newKey = "ca";
+  //      }
+  //      obj[newKey] = curr[key];
+  //     });
+  //      obj.status = "not filled";
+  //       obj.encrypt= "";
+  //     acc.push(obj);
+  //     return acc;
+  //   }, []);
+
+  //   // console.log(result);
+  //   setMarksTableData(result)
+  // }
+
+ 
+  const handleXls = () => {
+    setActive(0);
+  };
+  const handleCSV = () => {
+    setActive(1);
+  };
 
   return (
     <div>
@@ -71,8 +169,8 @@ const EncryptionCaPage = () => {
               "flex items-center justify-center gap-1 border-dashed border border-primary bg-[#bfdffb] py-[15px] mt-4 rounded-lg "
             }
           >
-            {selectedFile ? (
-              <h1 className="text-2xl">{selectedFile.name}</h1>
+            {fileName ? (
+              <h1 className="text-2xl">{fileName.name}</h1>
             ) : (
               <>
                 <h1 className="text-xl">
@@ -99,10 +197,7 @@ const EncryptionCaPage = () => {
         </label>
       </div>
 
-
-
-
-      <EncryptCaTable/>
+      <EncryptCaTable marksTableData={marksTableData} setMarksTableData={setMarksTableData} />
     </div>
   );
 }
